09db8c9b70bd0b887e956a0ecdb5b3f524f2e0d2
Matt Smith <mattonfoot@gmail.com>
2017-12-12 12:24:23 +0000

    add GA tracking codes

--- test/api/apiDocs.js
+++ test/api/apiDocs.js
@@ -16,24 +16,30 @@ describe('api /api-docs', () => {
   });
 
   describe('GET /api-docs', () => {
-
-    it('should return a 200 response with some content', () => {
+    it('should return a json response', () => {
       return request(server)
         .get('/api-docs')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
-        .expect(200)
-        .then((res) => {
+        .expect(200);
+    });
 
+    it('should return a valid swagger 2.0 document', () => {
+      return request(server)
+        .get('/api-docs')
+        .set('Accept', 'application/json')
+        .then((res) => {
           return new Promise((resolve, reject) => {
             require('swagger-tools').specs.v2_0.validate(res.body, (err, results) => {
               if (err) return reject(err);
+
               if (results && results.errors && results.errors.length > 0) {
                 const error = results.errors
                   .map(({message, path}) => `${message} at ${path.join(".")}`)
                   .join("\n");
                 return reject(new Error("Invalid Swagger:\n" + error));
               }
+
               resolve("ok");
             });
           });
