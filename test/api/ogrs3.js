b169d477dc241434b3ba526ff98842d46fd8e57b
Matt Smith <mattonfoot@gmail.com>
2017-12-18 11:13:28 +0000

    upgrade node, update OGRS3 results, include package-lock

--- test/api/ogrs3.js
+++ test/api/ogrs3.js
@@ -33,7 +33,7 @@ describe('api /calculate/ogrs3', () => {
       .then((res) => {
         res.body.should.have.property('calculatorVersion');
         res.body.should.have.property('OGRS3');
-        res.body.OGRS3.should.eql([ 0.10966125120067906, 0.20180313320047943 ]);
+        res.body.OGRS3.should.eql([ 0.10966125120067907, 0.20180313320047938 ]);
         res.body.should.have.property('OGRS3PercentileRisk');
         res.body.OGRS3PercentileRisk.should.eql([ 11, 20 ]);
       });
